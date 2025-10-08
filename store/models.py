326caf1ac6c99b7104f3dde607ddd2d609dfa62c
shivanshvermaaa <shivanshvermaaa@gmail.com>
2025-10-09 00:10:08 +0530

    created base template and populated the databases and initiated designing the category view

--- store/models.py
+++ store/models.py
@@ -12,6 +12,9 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
+    # FK 
+    category= models.ForeignKey(Category,related_name='product',on_delete=models.CASCADE,null=True)
+    
     title = models.CharField(max_length=250)
     brand = models.CharField(max_length=250, default='un-branded')
     description= models.TextField(blank=True)
