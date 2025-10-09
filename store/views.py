6857c760cff6f8fcbdb950ca3f696d7fb786fab6
shivanshvermaaa <shivanshvermaaa@gmail.com>
2025-10-09 18:15:07 +0530

    created singluar product page

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Category,Product
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def store(request):
     all_products= Product.objects.all()
@@ -9,4 +10,9 @@ def store(request):
 def categories(request):
     all_categories= Category.objects.all()
     return {'all_categories':all_categories}
+
+def product_info(request, slug):
+    product= get_object_or_404(Product, slug=slug)
+    context={'product':product,}
+    return render(request, 'store/product-info.html', context=context)
 
