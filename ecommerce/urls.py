aea13fc9ed461b3f0dd674b7e2692fcba04cf29e
shivanshvermaaa <shivanshvermaaa@gmail.com>
2025-10-11 04:19:02 +0530

    made the account app and linked to the main URL

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("",include('store.urls')),
     path("cart/", include('cart.urls')),# new
+    path("account/", include('account.urls')), # new
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
